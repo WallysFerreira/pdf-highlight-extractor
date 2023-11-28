08595148b8572739ce71aba7f4d9355b9fe8e63b
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-28 08:45:57 -0300

    cortar PDF na marcação e criar PDF novo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pypdf import PdfReader
+from pypdf import PdfReader, PdfWriter
 from pdf2image import convert_from_path
 from PIL import Image
 import tempfile
@@ -9,10 +9,11 @@ class AnotacaoEncontrada:
     def __init__(self) -> None:
         self.text = ""
         self.pagina = 0
+        self.numero = 0
         self.coordenadas = []
         pass
 
-anotacoes_encontradas = []
+numero_anotacoes_encontradas = 1
 
 for numero_pagina, pagina in enumerate(leitor.pages):
     # Algumas páginas não tem anotação nenhuma
@@ -24,9 +25,12 @@ for numero_pagina, pagina in enumerate(leitor.pages):
 
             # A anotação só é valida se tiver o campo QuadPoints
             if "/QuadPoints" in anotacao:
+                escritor = PdfWriter()
                 coordenadas_cantos_retangulo = anotacao["/QuadPoints"]
                 anotacao_encontrada = AnotacaoEncontrada()
                 anotacao_encontrada.pagina = numero_pagina + 1
+                anotacao_encontrada.numero = numero_anotacoes_encontradas
+                numero_anotacoes_encontradas += 1
 
                 if len(coordenadas_cantos_retangulo) > 8:
                     for i in range(len(coordenadas_cantos_retangulo) // 8):
@@ -45,7 +49,14 @@ for numero_pagina, pagina in enumerate(leitor.pages):
 
                     anotacao_encontrada.coordenadas.append([canto_esquerdo_superior, canto_direito_inferior])
 
-                anotacoes_encontradas.append(anotacao_encontrada)
+                for coord in anotacao_encontrada.coordenadas:
+                    print(anotacao_encontrada.coordenadas)
+                    print(coord)
+                    pagina.mediabox.upper_left = (coord[0][0], coord[0][1])
+                    pagina.mediabox.lower_right = (coord[1][0], coord[1][1])
+
+                    escritor.add_page(pagina)
+                    escritor.write(f'{anotacao_encontrada.pagina}_{anotacao_encontrada.numero}.pdf')
 
 with tempfile.TemporaryDirectory() as path:
     imagens = convert_from_path('teste.PDF', output_folder=path)
@@ -53,4 +64,3 @@ with tempfile.TemporaryDirectory() as path:
     for numero_pagina, imagem in enumerate(imagens):
         imagem.save(f"{path}/{numero_pagina}.jpg")
 
-    for anotacao in anotacoes_encontradas:
