344e5e7c4bebcca8d8f9f79b6e7cd07db2e445c4
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-27 21:48:32 -0300

    usar arquivo de teste para extrair imagens

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ for numero_pagina, pagina in enumerate(leitor.pages):
                 anotacoes_encontradas.append(anotacao_encontrada)
 
 with tempfile.TemporaryDirectory() as path:
-    imagens = convert_from_path('/home/belval/example.pdf', output_folder=path)
+    imagens = convert_from_path('teste.PDF', output_folder=path)
+
     for numero_pagina, imagem in enumerate(imagens):
         imagem.save(f"{path}/{numero_pagina}.jpg")
