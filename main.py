cd88dd1fb2f7b8efa6f0ffbf6357cc84804e2d28
Wallys Ferreira <wallysfsilva@gmail.com>
2023-11-28 19:31:54 -0300

    tirar threads da função principal

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class AnotacaoEncontrada:
         self.numero = 0
         self.coordenadas = []
 
-def extrair(caminho_arquivo_entrada):
+def extrair(caminho_arquivo_entrada, caminho_arquivo_saida):
     leitor = PdfReader(caminho_arquivo_entrada)
     arquivo_saida = open('saida.txt', 'w')
     ultima_pagina_mostrada = 0
@@ -74,51 +74,27 @@ def extrair(caminho_arquivo_entrada):
 
                     anotacoes_encontradas.append(anotacao_encontrada)
 
-                    """
-                    thread = threading.Thread(target=cortar_e_salvar, args=(pagina, anotacao_encontrada.coordenadas, f'{path.name}/{anotacao_encontrada.pagina}_{anotacao_encontrada.numero}.pdf'))
-                    #cortar_e_salvar(pagina, anotacao_encontrada.coordenadas, f'{path.name}/{anotacao_encontrada.pagina}_{anotacao_encontrada.numero}.pdf')
+    for anotacao_encontrada in anotacoes_encontradas:
+        cortar_e_salvar(paginas_pdf[anotacao_encontrada.pagina - 1], anotacao_encontrada.coordenadas, f'{path.name}/{anotacao_encontrada.pagina}_{anotacao_encontrada.numero}.pdf')
 
-                    imagens = convert_from_path(f'{path.name}/{anotacao_encontrada.pagina}_{anotacao_encontrada.numero}.pdf', output_folder=path.name)
+        imagens = convert_from_path(f'{path.name}/{anotacao_encontrada.pagina}_{anotacao_encontrada.numero}.pdf', output_folder=path.name)
 
-                    for imagem in imagens:
-                        texto_extraido = pytesseract.image_to_string(imagem, lang="por").strip()
+        for imagem in imagens:
+            texto_extraido = pytesseract.image_to_string(imagem, "por").strip()
 
-                        anotacao_encontrada.texto += texto_extraido
-                        anotacao_encontrada.texto += " "
+            anotacao_encontrada.texto += texto_extraido
+            anotacao_encontrada.texto += " "
 
-                    if ultima_pagina_mostrada != anotacao_encontrada.pagina:
-                        print()
-                        arquivo_saida.write("\n")
-                        print("Pagina", anotacao_encontrada.pagina)
-                        arquivo_saida.write(f"Pagina {anotacao_encontrada.pagina}\n" )
-                        ultima_pagina_mostrada = anotacao_encontrada.pagina
+        if ultima_pagina_mostrada != anotacao_encontrada.pagina:
+            print()
+            arquivo_saida.write("\n")
+            print("Pagina", anotacao_encontrada.pagina)
+            arquivo_saida.write(f"Pagina {anotacao_encontrada.pagina}\n" )
+            ultima_pagina_mostrada = anotacao_encontrada.pagina
 
-                    print("Anotação", anotacao_encontrada.numero)
-                    arquivo_saida.write(f"Anotação {anotacao_encontrada.numero}\n")
-                    print(anotacao_encontrada.texto)
-                    arquivo_saida.write(f"{anotacao_encontrada.texto}\n")
-"""
+        print("Anotação", anotacao_encontrada.numero)
+        arquivo_saida.write(f"Anotação {anotacao_encontrada.numero}\n")
+        print(anotacao_encontrada.texto)
+        arquivo_saida.write(f"{anotacao_encontrada.texto}\n")
 
-import time
-extrair('./teste.PDF')
-#print(anotacoes_encontradas)
-
-tempo_comeco_thread = time.time()
-array_anotacoes_comeco = anotacoes_encontradas[len(anotacoes_encontradas) // 2 + 1:]
-array_anotacoes_final = anotacoes_encontradas[:len(anotacoes_encontradas) // 2]
-
-for anotacao in array_anotacoes_comeco:
-    threading.Thread(target=cortar_e_salvar, args=(paginas_pdf[anotacao.pagina - 1], anotacao.coordenadas, f'{anotacao.pagina}_{anotacao.numero}.pdf')).start()
-
-for anotacao in array_anotacoes_final:
-    threading.Thread(target=cortar_e_salvar, args=(paginas_pdf[anotacao.pagina - 1], anotacao.coordenadas, f'{anotacao.pagina}_{anotacao.numero}.pdf')).start()
-
-tempo_final_thread = time.time()
-
-tempo_comeco_sem_thread = time.time()
-for anotacao in anotacoes_encontradas:
-    cortar_e_salvar(paginas_pdf[anotacao.pagina - 1], anotacao.coordenadas, f'{anotacao.pagina}_{anotacao.numero}.pdf')
-tempo_final_sem_thread = time.time()
-
-print("Tempo total thread:", tempo_final_thread - tempo_comeco_thread)
-print("Tempo total sem thread:", tempo_final_sem_thread - tempo_comeco_sem_thread)
+extrair('./teste2.pdf', './saida.txt')
