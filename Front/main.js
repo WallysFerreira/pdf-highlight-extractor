45dd9a9602ddb85e68baf4d3875077e4cea3c35c
Gleice Santos <116775112+gleicesantosdev@users.noreply.github.com>
2023-11-28 22:40:02 -0300

    atualização da barra de progresso

--- Front/main.js
+++ Front/main.js
@@ -1,6 +1,8 @@
 const inputFile = document.querySelector("#file");
 const imgArea = document.querySelector(".img-area");
 const pictureImageTxt = "Carregue o Arquivo";
+const progressBar = document.querySelector("#progress-bar"); // Add uma barra de progresso no seu HTML
+const progressText = document.querySelector("#progress-text"); // Add um elemento de texto p mostrar o progresso
 
 document.querySelector('.icon').addEventListener('click', function() {
     inputFile.click();
@@ -26,8 +28,17 @@ inputFile.addEventListener("change", function (e) {
       imgArea.appendChild(object);
     });
 
+    reader.addEventListener("progress", function (e) {
+      if (e.lengthComputable) {
+        const percentLoaded = Math.round((e.loaded / e.total) * 100);
+        progressBar.value = percentLoaded; // Atualiza o valor da barra de progresso
+        progressText.textContent = `Carregando ${file.name}: ${percentLoaded}%`; // Atualiza o texto do progresso
+      }
+    });
+
     reader.readAsDataURL(file);
   } else {
     imgArea.innerHTML = pictureImageTxt;
+    progressText.textContent = ""; // Limpa o texto do progresso
   }
 });
