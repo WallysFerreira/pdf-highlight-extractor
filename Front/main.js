af4bf0075d9907e59689b6718219103aee54f7ea
Gleice Santos <116775112+gleicesantosdev@users.noreply.github.com>
2023-11-28 23:17:45 -0300

    adicionando envio do arquivo para api no js

--- Front/main.js
+++ Front/main.js
@@ -2,7 +2,7 @@ const inputFile = document.querySelector("#file");
 const imgArea = document.querySelector(".img-area");
 const pictureImageTxt = "Carregue o Arquivo";
 const progressBar = document.querySelector("#progress-bar"); // Add uma barra de progresso no seu HTML
-const progressText = document.querySelector("#progress-text"); // Add um elemento de texto p mostrar o progresso
+const progressText = document.querySelector("#progress-text"); // Add um elemento de texto para mostrar o progresso
 
 document.querySelector('.icon').addEventListener('click', function() {
     inputFile.click();
@@ -34,6 +34,25 @@ inputFile.addEventListener("change", function (e) {
         progressBar.value = percentLoaded; // Atualiza o valor da barra de progresso
         progressText.textContent = `Carregando ${file.name}: ${percentLoaded}%`; // Atualiza o texto do progresso
       }
+    });
+
+    reader.addEventListener("loadend", function () {
+      // Envia o arquivo para a API dps do carregamento
+      const formData = new FormData();
+      formData.append('file', file);
+
+      fetch('https://pdf-highlight-extractor-api.onrender.com/extract', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => { //correção aqui, aqui processa a resposta da api
+        console.log(data);
+        // Processa a resposta da API aqui
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
     });
 
     reader.readAsDataURL(file);
